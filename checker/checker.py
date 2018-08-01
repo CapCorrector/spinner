f276a1c52adf637d0ef885ce9509a9252144b5e7
Corrector <avanger@mail.ru>
2018-08-01 00:58:12 -0700

    Don't run on import

--- checker/checker.py
+++ checker/checker.py
@@ -1,14 +1,15 @@
-import socket
+if __name__ == '__main__':
+    import socket
 
-HOST, PORT = "127.0.0.1", 9998
-data = "get_cpu"
+    HOST, PORT = "127.0.0.1", 9998
+    data = "get_cpu"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.settimeout(5)
-    sock.connect((HOST, PORT))
-    sock.sendall(bytes(data + "\n", "utf-8"))
-    received = str(sock.recv(1024), "utf-8")
-    sock.close()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.settimeout(5)
+        sock.connect((HOST, PORT))
+        sock.sendall(bytes(data + "\n", "utf-8"))
+        received = str(sock.recv(1024), "utf-8")
+        sock.close()
 
-print("Sent:     {}".format(data))
-print("Received: {}".format(received))
+    print("Sent:     {}".format(data))
+    print("Received: {}".format(received))
